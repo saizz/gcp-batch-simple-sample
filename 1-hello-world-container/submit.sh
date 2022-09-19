a14922d2ecaf04db86eeacdc4a9656574615d5c0
Hiroshi Saito <h1r0sh1.sa1t0w@gmail.com>
2022-09-19 19:34:34 +0900

    add: sample hello-world-container

---
+++ 1-hello-world-container/submit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+set -eu
+
+project="saizo-batch-sample"
+location="us-central1"
+job="example-container-job"
+
+gcloud beta batch jobs submit ${job} --location=${location} --config=config.json
+
+job_name="projects/${project}/locations/${location}/jobs/${job}"
+echo "$(date "+%H:%M:%S") job name: ${job_name}"
+
+while :
+do
+  state="$(gcloud beta batch jobs describe ${job_name} --format=json | jq .status.state)"
+  echo "$(date "+%H:%M:%S") state: ${state}"
+  sleep 5s
+done
